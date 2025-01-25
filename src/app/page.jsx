eb7756be87019a8f371e38c4107368a56eba6fb6
Justin Null <null@Justins-MacBook-Pro.local>
2025-01-25 11:52:29 -0500

    text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>   
-          <h1 className="text-2xl font-extrabold">www.justin-null.com</h1>
+          <h1 className="text-6xl font-thin">
+            <span className="opacity-25">www.</span>
+            <span>justin</span>
+            <span className="opacity-25">-</span>
+            <span>null</span>
+            <span className="opacity-25">.com</span>
+          </h1>
         </div>
       </main>
     </div>
